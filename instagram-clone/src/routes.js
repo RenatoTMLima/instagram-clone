79a1816e24984c2bf4430d7ae9cf484acc1ec40b
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-01-24 14:26:16 -0300

    Made until 32 24 from the mobile app video

---
+++ instagram-clone/src/routes.js
@@ -0,0 +1,24 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {Image} from 'react-native';
+import React from 'react';
+
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+import logo from './assets/logo.png';
+
+export default createAppContainer(
+   createStackNavigator({
+      Feed,
+      New
+   },{
+      defaultNavigationOptions: {
+         headerTintColor: '#000',
+         headerTitle: () => <Image source={logo} />,
+         headerTitleAlign: 'center',
+         headerBackTitle: null
+      },
+      mode: 'modal'
+   })
+)
